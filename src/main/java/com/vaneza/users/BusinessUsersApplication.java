96472368d71578fc0569fc82949a976cb922c376
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-06-06 09:13:13 -0500

    chore: inital commit

---
+++ src/main/java/com/vaneza/users/BusinessUsersApplication.java
@@ -0,0 +1,13 @@
+package com.vaneza.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessUsersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BusinessUsersApplication.class, args);
+  }
+
+}
